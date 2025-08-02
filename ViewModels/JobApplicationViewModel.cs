19744b50ab4285070728c0cbd7d7986b24bf6b45
Mario Dawod <mariodawod@Marios-MacBook-Air.local>
2025-08-03 00:43:58 +0300

    ..

--- ViewModels/JobApplicationViewModel.cs
+++ ViewModels/JobApplicationViewModel.cs
@@ -3,14 +3,10 @@ using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 public class JobApplicationViewModel
 {
-    [ForeignKey("Job")]
-    public int JobId { set; get; }
-    [ValidateNever]
-    public Job Job { set; get; }
-    [ForeignKey("ApplicationUser")]
+    [Required]
+    public int JobId { set; get; }  
+    [Required]
     public string UserId { set; get; }
-    [ValidateNever]
-    public ApplicationUser User { set; get; }
     [Required]
     public IFormFile cvFile{ set; get; }
     public string coverLetter{ set; get; }
